c90ff644ecf362d4375e1ca3fbad69c7576fa557
MosheBoord <mcboord@gmail.com>
2021-08-31 21:49:06 -0400

    updated landing page and added navbar

--- app/main.js
+++ app/main.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import LandingPage from "./views/landing-page";
+import { HashRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/navbar";
+// eslint-disable-next-line no-unused-vars
+import { Dropdown } from "bootstrap";
 
-render(<LandingPage></LandingPage>, document.getElementById("main"));
+render(
+	<Router>
+		<NavBar />
+		<Route exact path="/" component={LandingPage} />
+	</Router>,
+	document.getElementById("main")
+);
--- app/views/landing-page.js
+++ app/views/landing-page.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const LandingPage = () => (
-	<div>
-		<h1>Hello!</h1>
-	</div>
-);
+const LandingPage = () => {
+	let i = 500;
+	const emojis = ["🦆", "😊", "💎", "💩", "⬜"];
+	const arr = [];
+	for (i; i > 0; i--) {
+		// arr.push(emojis[i % emojis.length]);
+		arr.push(emojis[Math.floor(Math.random() * emojis.length)]);
+	}
+	return (
+		<div>
+			{/* <h1>Hello! {arr}</h1> */}
+			<img src="img/sample-product.jfif"></img>
+		</div>
+	);
+};
 
 export default LandingPage;
